3ebe268771ac752d804b53598775a530b92d80a0
Rahul Choudhury <rchoudhury63@gmail.com>
2026-01-07 11:25:46 +0530

    refactor: simplify opengraph images with curly brace title style
    Remove SVG icons and wrap titles in curly braces to match the site's
    PageTitle component styling. Use Instrument Serif font consistently.

--- app/[slug]/opengraph-image.tsx
+++ app/[slug]/opengraph-image.tsx
@@ -24,10 +24,10 @@ export default async function Image({
 }) {
   const { slug } = await params;
 
-  const [{ data: blog }, interFont] = await Promise.all([
+  const [{ data: blog }, instrumentSerifFont] = await Promise.all([
     api.get<Blog>(`/api/blogs/details?slug=${slug}`),
     fetch(
-      "https://fonts.gstatic.com/s/inter/v20/UcCO3FwrK3iLTeHuS_nVMrMxCp50SjIw2boKoduKmMEVuGKYMZg.ttf",
+      "https://fonts.gstatic.com/s/instrumentserif/v5/jizBRFtNs2ka5fXjeivQ4LroWlx-2zI.ttf",
     ).then((res) => res.arrayBuffer()),
   ]);
 
@@ -40,44 +40,31 @@ export default async function Image({
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        gap: 32,
         padding: 80,
       }}
     >
-      <svg
-        width="72"
-        height="72"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="#ff6d1f"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        style={{ flexShrink: 0 }}
-      >
-        <path d="M8 3H7a2 2 0 0 0-2 2v5a2 2 0 0 1-2 2 2 2 0 0 1 2 2v5c0 1.1.9 2 2 2h1" />
-        <path d="M16 21h1a2 2 0 0 0 2-2v-5c0-1.1.9-2 2-2a2 2 0 0 1-2-2V5a2 2 0 0 0-2-2h-1" />
-      </svg>
       <div
         style={{
+          display: "flex",
           fontSize: 56,
-          fontFamily: "Inter",
-          fontWeight: 600,
+          fontFamily: "Instrument Serif",
           color: "#faf3e1",
           lineHeight: 1.2,
         }}
       >
+        <span style={{ color: "#ff6d1f" }}>{"{"}</span>
         {blog.title}
+        <span style={{ color: "#ff6d1f" }}>{"}"}</span>
       </div>
     </div>,
     {
       ...size,
       fonts: [
         {
-          name: "Inter",
-          data: interFont,
+          name: "Instrument Serif",
+          data: instrumentSerifFont,
           style: "normal",
-          weight: 600,
+          weight: 400,
         },
       ],
     },
--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -21,30 +21,19 @@ export default async function Image() {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        gap: 32,
       }}
     >
-      <svg
-        width="72"
-        height="72"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="#ff6d1f"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M8 3H7a2 2 0 0 0-2 2v5a2 2 0 0 1-2 2 2 2 0 0 1 2 2v5c0 1.1.9 2 2 2h1" />
-        <path d="M16 21h1a2 2 0 0 0 2-2v-5c0-1.1.9-2 2-2a2 2 0 0 1-2-2V5a2 2 0 0 0-2-2h-1" />
-      </svg>
       <div
         style={{
+          display: "flex",
           fontSize: 72,
           fontFamily: "Instrument Serif",
           color: "#faf3e1",
         }}
       >
+        <span style={{ color: "#ff6d1f" }}>{"{"}</span>
         Destructure
+        <span style={{ color: "#ff6d1f" }}>{"}"}</span>
       </div>
     </div>,
     {
