a7b42747ddb52c62cc13c502231b349791a7b95d
Rahul Choudhury <rchoudhury63@gmail.com>
2026-01-17 19:19:52 +0530

    feat: decode jwt util using jose

--- lib/session.ts
+++ lib/session.ts
@@ -1,7 +1,30 @@
 import "server-only";
 import { cookies } from "next/headers";
+import { JWTPayload, jwtVerify } from "jose";
 
 export async function getTokenFromCookie() {
   const cookieStore = await cookies();
   return cookieStore.get("token")?.value;
+}
+
+interface JWTPayloadWithRole extends JWTPayload {
+  isAdmin: boolean;
+}
+
+const secretKey = process.env.JWT_SECRET;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+export async function decodeToken(jwtToken: string | undefined = "") {
+  try {
+    const { payload } = await jwtVerify<JWTPayloadWithRole>(
+      jwtToken,
+      encodedKey,
+      {
+        algorithms: ["HS256"],
+      },
+    );
+    return payload;
+  } catch {
+    return undefined;
+  }
 }
